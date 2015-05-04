ac6e9230778766ce3f0050774bfaa544d4b7382a
Ming Yang <mingy@mit.edu>
2015-05-04 14:32:11 -0400

    Written spec for instance methods in ExpressionListenerExpressionCreator

--- src/expresso/parser/ExpressionListenerExpressionCreator.java
+++ src/expresso/parser/ExpressionListenerExpressionCreator.java
@@ -9,26 +9,23 @@ import expresso.parser.ExpressionParser.*;
  * This class represents a listener that extends {@link ExpressionBaseListener}
  * An instance of ExpressionListenerExpressionCreator is passed into a parse
  * tree's walker method, and the listener methods are fired everytime the walk
- * visits a node of a specific type. Only a subset of nodes have listener methods
+ * visits a node of a specific type. Only a subset of nodes have listeners
  * attached. 
+ *
  * Upon concluding the walk, a tree of Expression nodes is created, and can be 
- * accessed by calling getExpression()
+ * accessed by calling getExpression() on the instance
  */
 public class ExpressionListenerExpressionCreator extends ExpressionBaseListener {
     
     private final Stack<Expression> stack = new Stack<Expression>();
     
     /**
-     * Do nothing when exiting root node.
+     * Tries to assert that the Expression tree is complete
+     * This is not a bijective condition; it only ensures that there are no
+     * dangling nodes, i.e. that there is only one node left that has no parent,
+     * namely, the root node.
      */
     public void exitRoot(RootContext ctx) {
-        assert stack.size() == 1;
-    }
-    
-    /**
-     * Do nothing when exiting file node.
-     */
-    public void exitFile(FileContext ctx) {
         assert stack.size() == 1;
     }
     
@@ -53,13 +50,17 @@ public class ExpressionListenerExpressionCreator extends ExpressionBaseListener 
     }
     
     /**
-     * Passes children into MultiplicationExpression 
      * If the node is a mult_expression, push it into the stack; this passes it
      * into its ancestor Expression's constructor. By design, the ancestor
      * Expression is either a MultiplicationExpression or an AdditionExpression.
+     *
+     * Also creates a MultiplicationExpression using the top two elements in the
+     * stack; this assigns the Expression's closest two descendents as its children
+     * The descendent expressions can be MultiplicationExpressions, Variables, or 
+     * Constants.
      * 
-     * Children expressions can be MultiplicationExpressions, Variables, or Constants.
-     * 
+     * This method is fired whenever the walker exits a mult_expression node
+     *
      * @param ctx mult_expression context
      */
     public void exitMultiplicationExpression(Mult_expressionContext ctx) {
@@ -70,9 +71,17 @@ public class ExpressionListenerExpressionCreator extends ExpressionBaseListener 
     }
     
     /**
-     * Passes children into AdditionExpression when exiting add_expression node.
-     * Children expressions can be MultiplicationExpressions, AdditionExpressions, Variables, or Constants.
+     * If the node is a add_expresison, push it into the stack; this passes it
+     * into its ancestor Expression's constructor. By design, the ancestor
+     * Expression is either a MultiplicationExpression or an AdditionExpression.
+     *
+     * Also creates an AdditionExpression using the top two elements in the stack;
+     * this assigns the Expression's closest two descendents as its children.
+     * The descendents can be AdditionExpressions, MultiplicationExpressions, 
+     * Variables, or Constants.
      * 
+     * This method is fired whenever the walker exits an add_expression node
+     *
      * @param ctx add_expression context
      */
     public void exitAdditionExpression(Add_expressionContext ctx) {
