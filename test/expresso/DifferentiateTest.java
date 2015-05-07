06e2079ebc3b1243eb4654ea54136e7816eed62e
Ming Yang <mingy@mit.edu>
2015-05-07 18:06:50 -0400

    Fixes two bugs in simplify, and adds tests to address them Also adds tests for differentiate

---
+++ test/expresso/DifferentiateTest.java
@@ -0,0 +1,59 @@
+package expresso;
+
+import static org.junit.Assert.*;
+
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+/**
+ * This class contains tests for the diffferentiate static method in the Expressions class.
+ */
+public class DifferentiateTest {
+    
+    /*
+     * Test strategy for differentiate
+     * 
+     * TODO
+     */
+    
+    @Test
+    public void testDifferentiateConstant() {
+        assertEquals(Expressions.differentiate("   5.3", "x"), "0.0");
+    }
+    
+    @Test
+    public void testDifferentiateVariable() {
+        assertEquals(Expressions.differentiate("foo  ","foo"), "1.0");
+    }
+    
+    @Test
+    public void testDifferentiateAddition() {
+        assertEquals(Expressions.differentiate("4.2 + foo", "foo"), "1.0");
+    }
+    
+    @Test
+    public void testDifferentiateIdentity() {
+        assertEquals(Expressions.differentiate("x + foo", "foo"), "1.0");
+    }
+    
+    
+    @Test
+    public void testDifferentiateMultiplication() {
+        assertEquals(Expressions.differentiate("foo* foo","foo"), "2.0*foo");
+    }
+    
+    @Test
+    public void testDifferentiateAdditionMultiplicationUnsimplified() {
+        assertEquals(Expressions.differentiate("4*(x*y + y*x)", "y"), "8.0*x");
+    }
+    
+    @Test
+    public void testDifferentiateAdditionMultiplication() {
+        assertEquals(Expressions.differentiate("4 + (x*x*x)", "x"), "3.0*x*x");
+    }
+    
+    @Test
+    public void testDifferentiateComplicated() {
+        assertEquals(Expressions.differentiate("4*(x*y + y*x + x*x*x)", "x"), "12.0*x*x+8.0*y");
+    }
+}
